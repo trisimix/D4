54367215fda3a753baf5b78fe22b373785933359
Josh Beaker <trisimix@protonmail.com>
2020-03-02 02:36:36 -0500

    removing dumb dumb files from failed gitignore(node modules and dist)

--- dist/middleware/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("./express/common");
-exports.default = [common_1.handleCors, common_1.handleBodyRequestParsing, common_1.handleCompression];
-//# sourceMappingURL=index.js.map
--- dist/types/database/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DbEnum;
-(function (DbEnum) {
-    DbEnum[DbEnum["lowdb"] = 0] = "lowdb";
-})(DbEnum = exports.DbEnum || (exports.DbEnum = {}));
-//# sourceMappingURL=index.js.map
